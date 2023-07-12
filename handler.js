7acbc2a64073edfdbdc5719a2a18f1026826242a
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-07-12 12:16:56 +0530

    Add update notes lambda

--- handler.js
+++ handler.js
@@ -28,12 +28,29 @@ module.exports.createNote = async (event, context, cb) => {
   }
 };
 
-module.exports.updateNote = async (event) => {
-  let notesID = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`Update notes with ID ${notesID}`),
-  };
+module.exports.updateNote = async (event, context, cb) => {
+  let notesId = event.pathParameters.id;
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: { notesId },
+      UpdateExpression: 'set #title = :title, #body= :body',
+      ExpressionAttributeNames: {
+        '#title': 'title',
+        '#body': 'body',
+      },
+      ExpressionAttributeValues: {
+        ':title': data.title,
+        ':body': data.body,
+      },
+      ConditionExpression: 'attribute_exists(notesId)',
+    };
+    await documentClient.update(params).promise();
+    cb(null, send(200, data));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
 };
 module.exports.deleteNote = async (event) => {
   let notesID = event.pathParameters.id;
