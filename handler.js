3a6c3d77cce38f02fa30c5d3f7bb668ee28d5665
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-07-12 11:47:37 +0530

    Add dynamo db

--- handler.js
+++ handler.js
@@ -1,10 +1,31 @@
 'use strict';
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const documentClient = new DynamoDB.DocumentClient({ region: 'ap-south-1' });
+const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
 
-module.exports.createNote = async (event) => {
+const send = (statusCode, data) => {
   return {
-    statusCode: 201,
-    body: JSON.stringify('Creating new note'),
+    statusCode,
+    body: JSON.stringify(data),
   };
+};
+module.exports.createNote = async (event, context, cb) => {
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body,
+      },
+      ConditionExpression: 'attribute_not_exists(notesId)',
+    };
+    await documentClient.put(params).promise();
+    cb(null, send(201, data));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
 };
 
 module.exports.updateNote = async (event) => {
